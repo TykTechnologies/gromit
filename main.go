cf17fb3e07b7d4b03fb56c0448782d181659eb4b
Alok G Singh <alephnull@hcoop.net>
2020-08-13 02:48:28 +0530

    max verbosity

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+package main
+
 /*
 Copyright © 2020 Alok G Singh <alok@tyk.io>
 
@@ -13,10 +15,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
 
 import "github.com/TykTechnologies/gromit/cmd"
 
 func main() {
-  cmd.Execute()
+	cmd.Execute()
 }
