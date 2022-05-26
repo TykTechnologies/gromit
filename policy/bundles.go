f554bc0cb5da121a1bc47691a6694846e85245eb
Asutosh <asutosh@tyk.io>
2022-05-26 17:09:35 +0530

    WIP: Fixed template name error.
    Also made the filepath split a bit more robust.

--- policy/bundles.go
+++ policy/bundles.go
@@ -6,7 +6,6 @@ import (
 	"html/template"
 	"io/fs"
 	"path/filepath"
-	"strings"
 
 	"github.com/rs/zerolog/log"
 )
@@ -15,29 +14,32 @@ import (
 var templates embed.FS
 
 // renderTemplates walks a bundle tree and calls renderTemplate for each path
-func (r *RepoPolicy) renderTemplates(dir string) error {
-	return fs.WalkDir(templates, dir, func(path string, d fs.DirEntry, err error) error {
+func (r *RepoPolicy) renderTemplates(bundleDir string) error {
+	return fs.WalkDir(templates, bundleDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return fmt.Errorf("Walk error: (%s): %v ", path, err)
 		}
 		if d.IsDir() {
 			return nil
 		}
-		return r.renderTemplate(path)
+		return r.renderTemplate(bundleDir, path)
 	})
 }
 
 // renderTemplate will render one template into its corresponding path in the git tree
 // The first two elements of the supplied path will be stripped to remove the templates/<bundle> to derive the
 // path that should be written to in the git repo.
-func (r *RepoPolicy) renderTemplate(path string) error {
-	pathElems := strings.Split(path, string(filepath.Separator))
-	opFile := filepath.Join(pathElems[2:]...)
+func (r *RepoPolicy) renderTemplate(bundleDir, path string) error {
+	opFile, err := filepath.Rel(bundleDir, path)
+	if err != nil {
+		return err
+	}
+	_, tName := filepath.Split(path)
 
 	op, err := r.gitRepo.CreateFile(opFile)
 	defer op.Close()
 	t := template.Must(template.
-		New(path).
+		New(tName).
 		Option("missingkey=error").
 		ParseFS(templates, path))
 	if err != nil {
