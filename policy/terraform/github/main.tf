b1eef06c5af10006c83fd06f19a2ae6c82e9c5fc
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-10-23 22:54:02 -0300

    uncomment add contexts

--- policy/terraform/github/main.tf
+++ policy/terraform/github/main.tf
@@ -53,6 +53,7 @@ module "tyk" {
     allows_force_pushes = false
     blocks_creations    = false
     contexts            = ["Go 1.16 Redis 5"]
+    //contexts            = ["test","Go 1.16 Redis 5","Analyze (go)","1.16", "lint","1.16-el7","ci","upgrade-deb (amd64, ubuntu:xenial)"," upgrade-deb (amd64, ubuntu:bionic)","upgrade-deb (amd64, ubuntu:focal)","upgrade-deb (amd64, debian:bullseye)","upgrade-deb (arm64, ubuntu:xenial)","upgrade-deb (arm64, ubuntu:bionic)"," upgrade-deb (arm64, ubuntu:focal)","upgrade-deb (arm64, debian:bullseye)","upgrade-rpm (ubi7/ubi)","upgrade-rpm (ubi8/ubi)","smoke-tests","CodeQL","SonarCloud","SonarCloud Code Analysis" ]
     review_count        = 2
     },
     {
@@ -88,61 +89,101 @@ module "tyk" {
   ]
 }
 
-# module "tyk-analytics" {
-#   # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
-#   source                          = "../../../modules/github-repos"
-#   repo                            = "tyk-analytics"
-#   description                     = "Tyk Dashboard New Repository"
-#   topics                          = []
-#   visibility                      = "private"
-#   default_branch                  = "master"
-#   required_status_checks_contexts = []
-#   required_approving_review_count = "2"
-# }
+module "tyk-analytics" {
+  # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
+  source                          = "../../../modules/github-repos"
+  repo                            = "tyk-analytics"
+  description                     = "Tyk Dashboard New Repository"
+  topics                          = []
+  visibility                      = "private"
+  default_branch                  = "master"
+  branch_protection_conf_set = [{
+    pattern             = "master"
+    signed_commits      = false
+    linear_history      = false
+    allows_deletions    = false
+    allows_force_pushes = false
+    blocks_creations    = false
+    contexts            = []
+    review_count        = 2
+    }]
+}
 
-# module "tyk-pump" {
-#   # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
-#   source                          = "../../../modules/github-repos"
-#   repo                            = "tyk-pump"
-#   description                     = "Tyk Analytics Pump to move analytics data from Redis to any supported back end (multiple back ends can be written to at once)."
-#   topics                          = []
-#   wiki                            = false
-#   default_branch                  = "master"
-#   required_status_checks_contexts = []
-#   required_approving_review_count = "2"
-# }
+module "tyk-pump" {
+  # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
+  source                          = "../../../modules/github-repos"
+  repo                            = "tyk-pump"
+  description                     = "Tyk Analytics Pump to move analytics data from Redis to any supported back end (multiple back ends can be written to at once)."
+  topics                          = []
+  wiki                            = false
+  default_branch                  = "master"
+  branch_protection_conf_set = [{
+    pattern             = "master"
+    signed_commits      = false
+    linear_history      = false
+    allows_deletions    = false
+    allows_force_pushes = false
+    blocks_creations    = false
+    contexts            = []
+    review_count        = 2
+    }]
+}
 
-# module "tyk-sink" {
-#   # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
-#   source                          = "../../../modules/github-repos"
-#   repo                            = "tyk-sink"
-#   description                     = "Tyk Open Source API Gateway written in Go, supporting REST, GraphQL, TCP and gRPC protocols"
-#   topics                          = []
-#   visibility                      = "private"
-#   default_branch                  = "master"
-#   required_status_checks_contexts = []
-#   required_approving_review_count = "2"
-# }
+module "tyk-sink" {
+  # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
+  source                          = "../../../modules/github-repos"
+  repo                            = "tyk-sink"
+  description                     = "Tyk Open Source API Gateway written in Go, supporting REST, GraphQL, TCP and gRPC protocols"
+  topics                          = []
+  visibility                      = "private"
+  default_branch                  = "master"
+  branch_protection_conf_set = [{
+    pattern             = "master"
+    signed_commits      = false
+    linear_history      = false
+    allows_deletions    = false
+    allows_force_pushes = false
+    blocks_creations    = false
+    contexts            = []
+    review_count        = 2
+    }]
+}
 
-# module "tyk-identity-broker" {
-#   # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
-#   source                          = "../../../modules/github-repos"
-#   repo                            = "tyk-identity-broker"
-#   description                     = "Tyk Authentication Proxy for third-party login"
-#   topics                          = []
-#   default_branch                  = "master"
-#   required_status_checks_contexts = []
-#   required_approving_review_count = "2"
-# }
+module "tyk-identity-broker" {
+  # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
+  source                          = "../../../modules/github-repos"
+  repo                            = "tyk-identity-broker"
+  description                     = "Tyk Authentication Proxy for third-party login"
+  topics                          = []
+  default_branch                  = "master"
+  branch_protection_conf_set = [{
+    pattern             = "master"
+    signed_commits      = false
+    linear_history      = false
+    allows_deletions    = false
+    allows_force_pushes = false
+    blocks_creations    = false
+    contexts            = []
+    review_count        = 2
+    }]
+}
 
-# module "portal" {
-#   # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
-#   source                          = "../../../modules/github-repos"
-#   repo                            = "portal"
-#   description                     = "Portal is a full-featured developer portal, blog and CMS"
-#   topics                          = ["portal", "api-gateway"]
-#   visibility                      = "private"
-#   default_branch                  = "master"
-#   required_status_checks_contexts = []
-#   required_approving_review_count = "2"
-# }
+module "portal" {
+  # source                          = "git::https://github.com/TykTechnologies/gromit.git//modules/github-repos?ref=feat/td-1220/github-PaC-terraform"
+  source                          = "../../../modules/github-repos"
+  repo                            = "portal"
+  description                     = "Portal is a full-featured developer portal, blog and CMS"
+  topics                          = ["portal", "api-gateway"]
+  visibility                      = "private"
+  default_branch                  = "master"
+  branch_protection_conf_set = [{
+    pattern             = "master"
+    signed_commits      = false
+    linear_history      = false
+    allows_deletions    = false
+    allows_force_pushes = false
+    blocks_creations    = false
+    contexts            = []
+    review_count        = 2
+    }]
+}
