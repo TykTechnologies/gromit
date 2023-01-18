9aaabe114287091ee2ff6ada79ce5a248bfbd572
Asutosh <asutosh@tyk.io>
2023-01-18 13:05:34 +0530

    Add test cases for releng version parsing.

--- policy/policy_test.go
+++ policy/policy_test.go
@@ -11,6 +11,8 @@ func TestPolicyConfig(t *testing.T) {
 	var rp Policies
 	config.LoadConfig("../testdata/policies/repos.yaml")
 	err := LoadRepoPolicies(&rp)
+	t.Logf("Branches: %+v", rp.Branches)
+	t.Logf("Branches.branch: %+v", rp.Branches.Branch)
 	if err != nil {
 		t.Fatalf("Could not load policy: %v", err)
 	}
@@ -20,8 +22,35 @@ func TestPolicyConfig(t *testing.T) {
 	}
 	assert.EqualValues(t, repo.Protected, []string{"master", "release-3-lts", "release-4"})
 	// test if branch policy for master is set correctly.
-	assert.EqualValues(t, repo.Branchvals.UpgradeFromVer, "3.0.8")
+	assert.EqualValues(t, "3.0.8", repo.Branchvals.UpgradeFromVer)
 	t.Logf("Branchvals: %+v", repo.Branchvals)
 	// test if  branch policy is set correctly for master
-	assert.EqualValues(t, repo.Branchvals.GoVersion, "1.16")
+	assert.EqualValues(t, "1.16", repo.Branchvals.GoVersion)
+	// master doesn't have any explicit version set, so should be
+	// release-5.x (the common branch value)
+	assert.EqualValues(t, "release-5.x", repo.Branchvals.RelengVersion)
+
+	repo, err = rp.GetRepo("tyk", "https://github.com/tyklabs", "release-4")
+	if err != nil {
+		t.Fatalf("Could not get a repo: %v", err)
+	}
+	// release-4 has explicit value release-4.x set.
+	assert.EqualValues(t, "release-4.x", repo.Branchvals.RelengVersion)
+
+	repo, err = rp.GetRepo("tyk", "https://github.com/tyklabs", "release-4.3")
+	if err != nil {
+		t.Fatalf("Could not get a repo: %v", err)
+	}
+	// release-4.3 has sourcebranch set to release-4, and no explicit
+	// relengversion set, so should inherit the value of release-4
+	assert.EqualValues(t, "release-4.x", repo.Branchvals.RelengVersion)
+
+	repo, err = rp.GetRepo("tyk", "https://github.com/tyklabs", "release-4.3.1")
+	if err != nil {
+		t.Fatalf("Could not get a repo: %v", err)
+	}
+	// release-4.3.1 has sourcebranch set to release-4.3, and no explicit
+	// releng version set, so we should recursively go to the root source
+	// brnach and inherit it's releng version value.
+	assert.EqualValues(t, "release-4.x", repo.Branchvals.RelengVersion)
 }
