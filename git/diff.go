a2aea7b7fc2647abd1464eadc0666af6b4c28903
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2023-02-14 08:29:58 -0300

    Td 1439/gh output for diagnostician (#115)
    * add pretty print

--- git/diff.go
+++ git/diff.go
@@ -1,13 +1,23 @@
 package git
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 	"github.com/waigani/diffparser"
 )
+
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var LBlue = "\033[94m"
+var White = "\033[97m"
 
 func gitDiff(dir string) (string, error) {
 	var out bytes.Buffer
@@ -19,7 +29,36 @@ func gitDiff(dir string) (string, error) {
 		return "", fmt.Errorf("diff in %s: %w", dir, err)
 	}
 	log.Trace().Bytes("output", out.Bytes()).Str("dir", dir).Msg("git diff")
+
+	prettyPrint(out.String())
+
 	return out.String(), nil
+}
+
+func prettyPrint(out string) {
+
+	red := regexp.MustCompile(`^-[^-]{2}.*`)
+	green := regexp.MustCompile(`^\+[^\+]{2}.*`)
+	lblue := regexp.MustCompile(`^@@ .*`)
+	yellow := regexp.MustCompile(`(^-{3} .*)|(^\+{3} .*)|(^diff --git .*)|(^index [\d|\w]{7,8}..[\d|\w]{7,8}.*)`)
+
+	scanner := bufio.NewScanner(strings.NewReader(out))
+	for scanner.Scan() {
+
+		if green.MatchString(scanner.Text()) {
+			fmt.Println(Green + scanner.Text())
+		} else if red.MatchString(scanner.Text()) {
+			fmt.Println(Red + scanner.Text())
+		} else if lblue.MatchString(scanner.Text()) {
+			fmt.Println(LBlue + scanner.Text())
+		} else if yellow.MatchString(scanner.Text()) {
+			fmt.Println(Yellow + scanner.Text())
+		} else {
+			fmt.Println(Reset + scanner.Text())
+		}
+
+	}
+
 }
 
 func parseDiff(ds string) ([]string, error) {
