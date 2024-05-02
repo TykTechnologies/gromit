6b52cea271dc52ec3136e1202f3828fed88dc204
Alok G Singh <alok@tyk.io>
2024-05-02 14:48:22 +0800

    Fetch stories of epics (#306)

--- policy/jira.go
+++ policy/jira.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	v3 "github.com/ctreminiom/go-atlassian/jira/v3"
+	"github.com/ctreminiom/go-atlassian/pkg/infra/models"
 	"github.com/rs/zerolog/log"
 )
 
@@ -37,8 +38,8 @@ func NewJiraClient(email, token string) *JiraClient {
 // understands a few types. Unknown content types are ignored.
 func (j *JiraClient) GetIssue(id string) (*JiraIssue, error) {
 	j.ctx = context.Background()
-	i, resp, err := j.c.Issue.Get(j.ctx, id, []string{"summary", "description", "subtasks"}, nil)
-	log.Trace().Interface("resp", resp).Interface("issue", i).Msg("getissue response")
+	i, resp, err := j.c.Issue.Get(j.ctx, id, []string{"summary", "description", "subtasks", "issuetype"}, nil)
+	log.Trace().Fields(resp).Interface("issue", i).Msg("getissue response")
 	if err != nil {
 		return nil, err
 	}
@@ -63,12 +64,36 @@ func (j *JiraClient) GetIssue(id string) (*JiraIssue, error) {
 		default:
 		}
 	}
-	for _, st := range i.Fields.Subtasks {
-		b += fmt.Sprintf("- [x] %s\n", st.Fields.Summary)
+	if i.Fields.IssueType.Name == "Epic" {
+		jql := fmt.Sprintf("parent = %s", id)
+		cis, resp, err := j.c.Issue.Search.Get(j.ctx, jql, nil, nil, 0, 20, "stories")
+		log.Trace().Fields(resp).Interface("issue", i).Msg("searching for children")
+		if err != nil {
+			log.Error().Err(err).Msgf("error fetching children of %s", id)
+		} else {
+			log.Debug().Msgf("found %d children", cis.Total)
+			b += getChildLines(cis.Issues)
+		}
 	}
+	b += getChildLines(i.Fields.Subtasks)
 	return &JiraIssue{
 		Id:    i.Key,
 		Title: i.Fields.Summary,
 		Body:  b,
 	}, err
+}
+
+// getChildLines(parent) returns lines of the form
+// - [ ] summary text
+// with the checkbox filled in if the task/story is done
+func getChildLines(parent []*models.IssueScheme) string {
+	var b string
+	for _, child := range parent {
+		status := " "
+		if child.Fields.Status.StatusCategory.Name == "Done" {
+			status = "x"
+		}
+		b += fmt.Sprintf("- [%s] %s\n", status, child.Fields.Summary)
+	}
+	return b
 }
