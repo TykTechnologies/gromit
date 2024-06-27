8bcfb87a5e36c6772f3f889af2647fd6db7c9e0c
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2024-06-27 14:00:14 -0300

    SYSE-364/add-tyk-ci-and-ui-to-regresion-test (#318)
    * Fix controller UI new integration
    * Adds tyk-ci config.yml and doctor releng
    * Creates a new feature for tyk-ci in order to enable Quality gates for both UI and API regression tests
    * Fixes controller UI pump and sink element duplication
    * Removes unused notifications from UI regression tests
    * Fixes several path issues related to docker logs, xray and other steps

--- policy/bundle_test.go
+++ policy/bundle_test.go
@@ -3,6 +3,7 @@ package policy
 import (
 	"os"
 	"path/filepath"
+	"slices"
 	"testing"
 	"time"
 
@@ -13,29 +14,38 @@ import (
 // repos in the config file.
 // FIXME: Test (bundle, features, repo) in parallel
 func TestBundleRender(t *testing.T) {
-	featDirs, err := templates.ReadDir("templates")
-	if err != nil {
-		t.Fatalf("Error reading embedded fs: %v", err)
-	}
-	var features []string
-	for _, fd := range featDirs {
-		if fd.IsDir() && fd.Name() != "subtemplates" {
-			features = append(features, fd.Name())
-		}
-	}
 	var pol Policies
 	config.LoadConfig("")
-	err = LoadRepoPolicies(&pol)
+	err := LoadRepoPolicies(&pol)
 	if err != nil {
 		t.Fatalf("Unable to load repo policies: %v", err)
 	}
-	b, err := NewBundle(features)
-	if err != nil {
-		t.Logf("Unable to create bundle obj: %v", err)
+
+	// Iterate through each group->repo->branch  and get the exhaustive list of  features
+	// used by each group
+	groupFeatures := make(map[string][]string)
+	for g, group := range pol.Groups {
+		var features []string
+		features = append(features, group.Features...)
+		for _, repo := range group.Repos {
+			features = append(features, repo.Features...)
+			for _, branch := range repo.Branches {
+				features = append(features, branch.Features...)
+
+			}
+		}
+		slices.Sort(features)
+		groupFeatures[g] = slices.Compact(features)
+		t.Logf("all features used by group %s : %v", g, groupFeatures[g])
 	}
+
 	for grpName, grp := range pol.Groups {
+		b, err := NewBundle(groupFeatures[grpName])
+		if err != nil {
+			t.Logf("Unable to create bundle obj: %v", err)
+		}
 		for r := range grp.Repos {
-			t.Logf("testing repo %s from group %s with features %v", r, grpName, features)
+			t.Logf("testing repo %s from group %s with features %v", r, grpName, groupFeatures[grpName])
 			rp, err := pol.GetRepoPolicy(r)
 			if err != nil {
 				t.Logf("Error getting repo policy for repo: %s: %v", r, err)
