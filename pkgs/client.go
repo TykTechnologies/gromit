ca8597e2d3774f8ad52f9749a9b18eda9447c245
Alok G Singh <alok@tyk.io>
2025-02-28 18:42:10 +0800

    packagecloud.io cleanup
    This PR adds a new command (pkgs) hierarchy and corresponding config
    to clean up the repos on the basis of version (assuming semver),
    upload timestamp. An explicit list of versions can be excluded from
    being selected for deletion

---
+++ pkgs/client.go
@@ -0,0 +1,242 @@
+package pkgs
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/peterhellberg/link"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	pc "github.com/tyklabs/packagecloud/api/v1"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/time/rate"
+)
+
+type Client struct {
+	token   string
+	owner   string
+	limiter *rate.Limiter
+	ctx     context.Context
+}
+
+func NewClient(authToken, owner string, rps float64, burst int) *Client {
+	limiter := rate.NewLimiter(rate.Limit(rps), burst)
+	return &Client{authToken, owner, limiter, context.TODO()}
+}
+
+// pkgConfig models the config file
+type pkgConfig struct {
+	Exceptions    []string
+	VersionCutoff string
+	AgeCutoff     time.Duration
+}
+
+type Repos map[string]pkgConfig
+
+type pkgList chan pc.PackageDetail
+
+const pcPrefix = "https://packagecloud.io/api/v1/repos"
+
+// LoadPkgs returns a map of repos→config from the embedded or
+// supplied config file
+func LoadConfig() (*Repos, error) {
+	pkgs := make(Repos)
+	return &pkgs, viper.UnmarshalKey("pkgs", &pkgs)
+}
+
+// get makes a GET request to a packagecloud API and returns the next
+// page link. Requests are limited by the rate limiter set when the
+// client is initialised.
+func (c *Client) get(url string) (*http.Response, error, string) {
+	var buf bytes.Buffer
+	req, err := http.NewRequestWithContext(c.ctx, "GET", url, &buf)
+	if err != nil {
+		return nil, fmt.Errorf("http newrequest err: %v", err), ""
+	}
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Accept", "application/json")
+
+	req.SetBasicAuth(c.token, "")
+	err = c.limiter.Wait(c.ctx)
+	if err != nil {
+		return nil, err, ""
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if resp.StatusCode != http.StatusOK {
+		b, _ := io.ReadAll(resp.Body)
+		return resp, fmt.Errorf("invalid response: %s err: %q", resp.Status, b), ""
+	}
+	total := resp.Header.Get("Total")
+	perPage := resp.Header.Get("Per-Page")
+	totalInt, _ := strconv.Atoi(total)
+	perPageInt, _ := strconv.Atoi(perPage)
+
+	if total != "" && perPage != "" && totalInt > perPageInt {
+		webLink := link.ParseResponse(resp)
+		if n, ok := webLink["next"]; ok {
+			return resp, nil, n.URI
+		}
+
+	}
+	return resp, nil, ""
+}
+
+// AllPackages returns an errgroup that can be used by the caller to
+// collect errors and a channel to read PackageDetails from. Only
+// items that satisy the supplied filter are written to the channel.
+// The JSON array returned by packagecloud is parsed as documented in
+func (c *Client) AllPackages(repo string, filter *Filter) (pkgList, *errgroup.Group) {
+	ch := make(pkgList)
+	url := fmt.Sprintf("%s/%s/%s/packages.json", pcPrefix, c.owner, repo)
+	pkgs := new(errgroup.Group)
+	pkgs.Go(func() error {
+		defer close(ch)
+		// do until next is ""
+		for {
+			resp, err, next := c.get(url)
+			if err != nil {
+				return fmt.Errorf("http get err: %v", err)
+			}
+			defer resp.Body.Close()
+			decoder := json.NewDecoder(resp.Body)
+
+			// Stream and parse JSON array as shown in
+			// https://pkg.go.dev/encoding/json#example-Decoder.Decode-Stream
+
+			// read opening bracket
+			_, err = decoder.Token()
+			if err != nil {
+				return err
+			}
+			for decoder.More() {
+				var item pc.PackageDetail
+				err := decoder.Decode(&item)
+				if err != nil {
+					return fmt.Errorf("json parse err: %v", err)
+				}
+				filter.IncTotal()
+
+				if filter.Satisfies(item, time.Now()) {
+					ch <- item
+					filter.IncFiltered()
+				}
+			}
+			// read closing bracket
+			_, err = decoder.Token()
+			if err != nil {
+				return err
+			}
+			if next != "" {
+				url = next
+			} else {
+				break
+			}
+		}
+		return nil
+	})
+	return ch, pkgs
+}
+
+func getContentLength(url string) (int64, error) {
+	resp, err := http.Head(url)
+	if err != nil {
+		return 0, fmt.Errorf("failed to send HEAD request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("bad status for HEAD: %s", resp.Status)
+	}
+
+	size := resp.ContentLength
+	if size < 0 {
+		return 0, fmt.Errorf("content length not found")
+	}
+
+	return size, nil
+}
+
+// download a package into savedir/name/distro/
+func (c *Client) download(item pc.PackageDetail, savedir string) error {
+	dirpath := fmt.Sprintf("%s/%s/%s", savedir, item.Name, item.DistroVersion)
+	err := os.MkdirAll(dirpath, 0755)
+	if err != nil {
+		return fmt.Errorf("could not create %s: %v", dirpath, err)
+	}
+	dir, err := os.OpenRoot(dirpath)
+	if err != nil {
+		return fmt.Errorf("could not open %s: %v", dirpath, err)
+	}
+	fi, err := dir.Stat(item.Filename)
+	var fsSize int64 = 0
+	if err != nil && !os.IsNotExist(err) {
+		log.Warn().Err(err).Msgf("stat %s/%s", dirpath, item.Filename)
+	}
+	if err == nil {
+		fsSize = fi.Size()
+	}
+	size, err := getContentLength(item.DownloadURL)
+	if err != nil {
+		log.Warn().Err(err).Msgf("filesize %s", item.DownloadURL)
+	}
+	if fsSize != size {
+		f, err := dir.Create(item.Filename)
+		if err != nil {
+			return fmt.Errorf("could not create %s/%s: %v", dirpath, item.Filename, err)
+		}
+		resp, err := http.Get(item.DownloadURL)
+		if err != nil {
+			return fmt.Errorf("failed to download %s: %v", item.DownloadURL, err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("bad status fetching %s: %s", item.DownloadURL, resp.Status)
+		}
+
+		_, err = io.Copy(f, resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to write %s/%s: %v", dirpath, item.Filename, err)
+		}
+	} else {
+		log.Debug().Msgf("not downloading %s/%s as it is already downloaded", dirpath, item.Filename)
+	}
+
+	return nil
+}
+
+// Clean deletes the packages from packagecloud
+func (c *Client) Clean(pList pkgList, concurrency int, savedir string) error {
+	errChan := make(chan error)
+	defer close(errChan)
+	pkgs := new(errgroup.Group)
+	for range concurrency {
+		pkgs.Go(func() error {
+			for item := range pList {
+				fmt.Println(item.Name, item.DistroVersion, item.Filename)
+				err := c.download(item, savedir)
+				if err != nil {
+					errChan <- err
+				}
+			}
+			return nil
+		})
+	}
+	go func() {
+		for cleanErr := range errChan {
+			log.Error().Err(cleanErr).Msg("while cleaning")
+		}
+	}()
+
+	if err := pkgs.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
